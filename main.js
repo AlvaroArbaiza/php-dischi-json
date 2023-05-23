53375295278a8ba4a525c3d1a81f687200a0f28d
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-05-23 15:42:28 +0200

    Creazione metodo per chiamare l'array nel file php

--- main.js
+++ main.js
@@ -3,7 +3,21 @@ const { createApp } = Vue
     createApp({
         data() {
             return {
-                
+                pathServer: 'server.php',
+                data: ''
             }
+        },
+        methods: {
+            call() {
+                axios.get(this.pathServer)
+                .then( (res) => {
+
+                    this.data = res.data
+
+                })
+            }
+        },
+        mounted() {
+            this.call();
         }
     }).mount('#app')
